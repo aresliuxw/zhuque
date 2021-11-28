08a16d6323b2c340201d6870e01ed77b427e4f52
liuxiaowei_kl <446748651@qq.com>
2021-11-28 10:45:50 +0800

    init

---
+++ src/main/java/com/cn/liu/ZhuqueApplication.java
@@ -0,0 +1,13 @@
+package com.cn.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhuqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cn/liu/ZhuqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.liu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhuqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
